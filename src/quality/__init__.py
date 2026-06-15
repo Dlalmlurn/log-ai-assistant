196cf4263a757cbdc1c13c877b709959657be525
Dlalmlurn <dlalmlurn@users.noreply.github.com>
2026-06-15 11:54:22 +0800

    feat: add data quality reconciliation checks

--- src/quality/__init__.py
+++ src/quality/__init__.py
@@ -1,3 +1,13 @@
-from .data_quality import build_data_quality_metrics, load_manifest_rows
+from .data_quality import (
+    build_data_quality_metrics,
+    build_reconciliation_report,
+    load_manifest_rows,
+    verify_manifest_event_ids,
+)
 
-__all__ = ["build_data_quality_metrics", "load_manifest_rows"]
+__all__ = [
+    "build_data_quality_metrics",
+    "build_reconciliation_report",
+    "load_manifest_rows",
+    "verify_manifest_event_ids",
+]
